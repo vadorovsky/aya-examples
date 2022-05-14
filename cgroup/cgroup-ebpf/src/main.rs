b5db886050e4b07cc1025c3f3e77788937450951
Michal Rostecki <vadorovsky@gmail.com>
2022-05-14 12:09:09 +0200

    Add an example with cgroup
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

---
+++ cgroup/cgroup-ebpf/src/main.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![no_main]
+
+use aya_bpf::{
+    cty::c_char,
+    helpers::bpf_probe_read_kernel_str,
+    macros::{btf_tracepoint, map},
+    maps::PerCpuArray,
+    programs::BtfTracePointContext,
+};
+use aya_log_ebpf::info;
+
+const LOG_BUF_CAPACITY: usize = 64;
+
+#[repr(C)]
+pub struct Buf {
+    pub buf: [u8; LOG_BUF_CAPACITY],
+}
+
+#[map]
+pub static mut BUF: PerCpuArray<Buf> = PerCpuArray::with_max_entries(1, 0);
+
+#[btf_tracepoint(name = "cgroup_mkdir")]
+pub fn cgroup_mkdir(ctx: BtfTracePointContext) -> i32 {
+    match { try_cgroup_mkdir(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_cgroup_mkdir(ctx: BtfTracePointContext) -> Result<i32, i32> {
+    let path = unsafe {
+        let path: *const c_char = ctx.arg(1);
+        let buf = BUF.get_mut(0).ok_or(0)?;
+        let len =
+            bpf_probe_read_kernel_str(path as *const u8, &mut buf.buf).map_err(|e| e as i32)?;
+        core::str::from_utf8_unchecked(&buf.buf[..len])
+    };
+
+    info!(&ctx, "tracepoint cgroup_mkdir called: {}", path);
+
+    if path.starts_with("/sys/fs/cgroup") {
+        info!(&ctx, "cgroup");
+    }
+    if path.starts_with("/system.slice/docker") {
+        info!(&ctx, "docker cgroup");
+    }
+
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
---
+++ cgroup/cgroup/src/main.rs
@@ -0,0 +1,51 @@
+use aya::{include_bytes_aligned, Bpf};
+use aya::{programs::BtfTracePoint, Btf};
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::info;
+use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new()
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/cgroup"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/cgroup"
+    ))?;
+    BpfLogger::init(&mut bpf)?;
+    let btf = Btf::from_sys_fs()?;
+    let program: &mut BtfTracePoint = bpf.program_mut("cgroup_mkdir").unwrap().try_into()?;
+    program.load("cgroup_mkdir", &btf)?;
+    program.attach()?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
