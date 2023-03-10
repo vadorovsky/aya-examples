a8f0d99223f3cadd1827e631a8ccc630fdeed59d
Michal Rostecki <vadorovsky@protonmail.com>
2023-03-10 11:25:51 +0100

    cshim: build: Build the shim object with one clang command

--- cshim/cshim-ebpf/build.rs
+++ cshim/cshim-ebpf/build.rs
@@ -7,21 +7,12 @@ fn main() {
         .arg("-I")
         .arg("src/")
         .arg("-O2")
+        .arg("-emit-llvm")
         .arg("-target")
         .arg("bpf")
-        .arg("-emit-llvm")
         .arg("-c")
         .arg("-g")
         .arg("src/vmlinux_access.c")
-        .arg("-o")
-        .arg(format!("{out_dir}/vmlinux_access.ll"))
-        .status()
-        .expect("Failed to compile the C-shim");
-
-    let _ = Command::new("llc")
-        .arg("-march=bpf")
-        .arg("-filetype=obj")
-        .arg(format!("{out_dir}/vmlinux_access.ll"))
         .arg("-o")
         .arg(format!("{out_dir}/vmlinux_access.o"))
         .status()
@@ -29,7 +20,6 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={out_dir}");
     println!("cargo:rustc-link-lib=link-arg={out_dir}/vmlinux_access.o");
-    // println!("cargo:rustc-link-arg-bin=cshim={out_dir}/vmlinux_access.o");
     println!("cargo:rerun-if-changed=src/vmlinux_access.c");
     println!("cargo:rerun-if-changed=src/vmlinux_access.h");
     println!("cargo:rerun-if-changed=src/vmlinux.h");
