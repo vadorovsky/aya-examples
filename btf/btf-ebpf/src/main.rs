12c588700ca9105ed6c652db6c52d21fd6fc7014
Michal Rostecki <vadorovsky@protonmail.com>
2024-05-25 06:41:51 +0200

    Example with BTF

---
+++ btf/btf-ebpf/src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{macros::lsm, programs::LsmContext};
+use aya_log_ebpf::info;
+
+pub struct Foo {
+    a: i32,
+    b: i32,
+}
+
+pub fn get_a(foo: &Foo) -> i32 {
+    foo.a
+}
+
+pub fn get_b(foo: &Foo) -> i32 {
+    foo.b
+}
+
+#[lsm(hook = "file_open")]
+pub fn file_open(ctx: LsmContext) -> i32 {
+    match try_file_open(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
+    info!(&ctx, "lsm hook file_open called");
+
+    let foo = Foo { a: 34, b: 75 };
+    info!(&ctx, "a: {}", get_a(&foo));
+    info!(&ctx, "b: {}", get_b(&foo));
+
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
